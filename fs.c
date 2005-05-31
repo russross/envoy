c3b53e62aeedf5c4874e787c9775bb86fe72f0b2
Russ Ross <rgr22@cl.cam.ac.uk>
2005-05-31 14:07:58 +0000

    [project @ Arch-1:rgr22@cl.cam.ac.uk--2005%envoy--main--1.0--patch-2] commented the 9P protocol semantics

--- fs.c
+++ fs.c
@@ -17,6 +17,7 @@
 
 #define ERROR_BUFFER_LEN 80
 
+/* generate a qid record from a file stat record */
 static inline struct qid stat2qid(struct stat *info) {
     struct qid qid;
 
@@ -33,6 +34,7 @@ static inline struct qid stat2qid(struct stat *info) {
     return qid;
 }
 
+/* convert a u32 to a string, allocating the necessary storage */
 static inline char *u32tostr(u32 n) {
     char *res = GC_MALLOC_ATOMIC(11);
     assert(res != NULL);
@@ -41,6 +43,7 @@ static inline char *u32tostr(u32 n) {
     return res;
 }
 
+/* convert a unix stat record to a p9stat record */
 static int stat2p9stat(struct stat *info, struct p9stat **p9info, char *path) {
     struct p9stat *res = GC_NEW(struct p9stat);
     assert(res != NULL);
@@ -100,6 +103,7 @@ static int stat2p9stat(struct stat *info, struct p9stat **p9info, char *path) {
     return 0;
 }
 
+/* get the unix flags (for open or create) from a 9P mode */
 static inline int unixflags(u32 mode) {
     int flags;
     switch (mode & OMASK) {
@@ -125,6 +129,7 @@ static inline int unixflags(u32 mode) {
     return flags;
 }
 
+/* generate an error response with Unix errno errnum */
 static void rerror(struct message *m, u16 errnum, int line) {
     char buf[ERROR_BUFFER_LEN];
 
@@ -187,6 +192,28 @@ static void rerror(struct message *m, u16 errnum, int line) {
 
 /*****************************************************************************/
 
+/**
+ * client_tversion: initial handshake
+ *
+ * Arguments:
+ * - msize[4]: suggested maximum message size
+ * - version[s]: protocol version proposed by the client
+ *
+ * Return:
+ * - msize[4]: maximum message size
+ * - version[s]: protocol version to be used on this connection
+ *
+ * Semantics:
+ * - must be first message sent on a 9P connection
+ * - client cannot issue any further requests until Rversion received
+ * - tag must be NOTAG
+ * - msize includes all protocol overhead
+ * - response msize must be <= proposed msize
+ * - version string must start with "9P"
+ * - if client version is not understood, respond with version = "unknown"
+ * - all outstanding i/o on the connection is aborted
+ * - all existing fids are clunked
+ */
 struct transaction *client_tversion(struct transaction *trans) {
     struct Tversion *req = &trans->in->msg.tversion;
     struct Rversion *res = &trans->out->msg.rversion;
@@ -205,10 +232,45 @@ struct transaction *client_tversion(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tauth: check credentials to authorize a connection
+ *
+ * Arguments:
+ * - afid[4]: fid to use for authorization channel
+ * - uname[s]: username associated with this connection
+ * - aname[s]: file tree to access
+ *
+ * Return:
+ * - aqid[13]: qid associated with afid
+ *
+ * Semantics:
+ * - if authorization is not required, return Rerror
+ * - aqid identifies a file of type QTAUTH
+ * - afid is used to exchange (undefined) data to authorize connection
+ */
 struct transaction *client_tauth(struct transaction *trans) {
     failif(-1, ENOTSUP);
 }
 
+/**
+ * client_tattach: establish a connection
+ *
+ * Arguments:
+ * - fid[4]: proposed fid for the connection
+ * - afid[4]: authorization fid
+ * - uname[s]: username associated with this connection
+ * - aname[s]: file tree to access
+ *
+ * Return:
+ * - qid[13]: qid associated with fid
+ *
+ * Semantics:
+ * - afid must be properly initialized through auth and read/writes
+ * - afid must be NOFID if authorization is not required
+ * - uname and aname must be the same as in corresponding auth sequence
+ * - afid may be used for multiple attach calls with same uname/aname
+ * - error returned if fid is already in use
+ */
 struct transaction *client_tattach(struct transaction *trans) {
     struct Tattach *req = &trans->in->msg.tattach;
     struct Rattach *res = &trans->out->msg.rattach;
@@ -229,10 +291,63 @@ struct transaction *client_tattach(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tflush: abort a message
+ *
+ * Arguments:
+ * - oldtag[2]: tag of transaction to abort
+ *
+ * Return:
+ *
+ * Semantics:
+ * - server should respond to Tflush immediately
+ * - if oldtag is recognized, abort any pending reponse and discard that tag
+ * - always respond with Rflush, never Rerror
+ * - multiple Tflushes for a single tag must be answered in order
+ * - Rflush for any of multiple Tflushes answers all previous ones
+ * - client cannot reuse oldtag until Rflush is received
+ * - client must honor regular response received before Rflush, including
+ *   and server-side state change
+ */
 struct transaction *client_tflush(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_twalk: descend a directory hierarchy
+ *
+ * Arguments:
+ * - fid[4]: fid of starting point
+ * - newfid[4]: fid to be set to endpoint of walk
+ * - nwname[2]: number of elements to walk
+ * - nwname * wname[s]: elements to walk
+ *
+ * Return:
+ * - nwqid[2]: number of elements walked
+ * - nwqid * qid[13]: elements walked
+ *
+ * Semantics:
+ * - newfid must not be in use unless it is the same as fid
+ * - fid must represent a directory unless nwname is zero
+ * - fid must be valid and not opened by open or create
+ * - if full sequence of nwname elements is walked successfully, newfid will
+ *   represent the file that results, else newfid and fid are unaffected
+ * - if newfid is in use or otherwise illegal, Rerror will be returned
+ * - ".." represents parent directory; "." (as current directory) is not used
+ * - nwname can be zero; newfid will represent same file as fid
+ * - if newfid == fid then change in newfid happends iff change in fid
+ * - for nwname > 0:
+ *   - elements are walked in-order, elementwise
+ *   - fid must be a directory, user must have permission to search directory
+ *   - same restrictions apply to implicit fids created along the walk
+ *   - if first element cannot be walked, Rerror is returned
+ *   - else normal return with nwqid elements for the successful elements
+ *     of the walk: nwqid < nwname implies a failure at index nwqid
+ *   - nwqid cannot be zero unless nwname is zero
+ *   - newfid only affected if nwqid == nwname (success)
+ *   - walk of ".." in root directory is equivalent to walk of no elements
+ *   - maximum of MAXWELEM (16) elements in a single Twalk request
+ */
 struct transaction *client_twalk(struct transaction *trans) {
     struct Twalk *req = &trans->in->msg.twalk;
     struct Rwalk *res = &trans->out->msg.rwalk;
@@ -302,6 +417,36 @@ struct transaction *client_twalk(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_topen: prepare a fid for i/o on an existing file
+ *
+ * Arguments:
+ * - fid[4]: fid of file to open
+ * - mode[1]: file mode requested
+ *
+ * Return:
+ * - qid[13]: qid representing newly opened file
+ * - iounit[4]: maximum number of bytes returnable in single 9P message
+ *
+ * Semantics:
+ * - modes OREAD, OWRITE, ORDWR, OEXEC open file for read-only, write-only,
+ *   read and write, and execute respectively, to be checked against
+ *   permissions of file
+ * - if OTRUNC bit is set, file is to be truncated, requiring write permission
+ * - if file is append-only and permission granted for open, OTRUNC is ignored
+ * - if mode has ORCLOSE bit set, file is set to be removed on clunk, which
+ *   requires permission to remove file from the directory
+ * - all other bits in mode must be zero
+ * - if file is marked for exclusive use, only one client can have it open
+ * - it is illegal to write to, truncate, or remove-on-close a directory
+ * - all permissions checked at time of open; subsequent changes to file
+ *   permissions do not affect already-open file
+ * - fid cannot refer to already-opened or created file
+ * - iounit may be zero
+ * - if iounit is nonzero, it is max number of bytes guaranteed to be read from
+ *   or written to a file without breaking the i/o transfer into multiple 9P
+ *   messages
+ */
 struct transaction *client_topen(struct transaction *trans) {
     struct Topen *req = &trans->in->msg.topen;
     struct Ropen *res = &trans->out->msg.ropen;
@@ -336,6 +481,33 @@ struct transaction *client_topen(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tcreate: prepare a fid for i/o on a new file
+ *
+ * Arguments:
+ * - fid[4]: fid of directory in which file is to be created
+ * - name[s]: name of new file
+ * - perm[4]: permissions for new file
+ * - mode[1]: file mode for new file
+ *
+ * Return:
+ * - qid[13]: qid representing newly created file
+ * - iounit[4]: maximum number of bytes returnable in single 9P message
+ *
+ * Semantics:
+ * - requires write permission in directory represented by fid
+ * - owner of new file is implied user of the request
+ * - group of new file is same as directory represented by fid
+ * - permissions of new regular file are: perm & (~0666 | (dir.perm & 0666))
+ * - permissions of new directory are:    perm & (~0777 | (dir.perm & 0777))
+ * - newly opened file is opened according to mode, which is NOT checked
+ *   against perm
+ * - returned qid is for the new file, and fid is updated to be the new file
+ * - directories are created by setting DMDIR in perm
+ * - names "." and ".." are special; it is illegal to create these names
+ * - attempt to create an existing file will be rejected
+ * - all other file open modes and restrictions match Topen; same for iounit
+ */
 struct transaction *client_tcreate(struct transaction *trans) {
     struct Tcreate *req = &trans->in->msg.tcreate;
     struct Rcreate *res = &trans->out->msg.rcreate;
@@ -407,6 +579,31 @@ struct transaction *client_tcreate(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tread: transfer data from a file
+ *
+ * Arguments:
+ * - fid[4]: file/directory to read from
+ * - offset[8]: position (in bytes) from which to start reading
+ * - count[4]: number of bytes to read
+ *
+ * Return:
+ * - count[4]: number of bytes read
+ * - data[count]: bytes read
+ *
+ * Semantics:
+ * - fid must be open for reading
+ * - read starts offset bytes from the beginning
+ * - count returned may be equal or less than count requested
+ * - if offset is >= number of bytes in file, count of zero is returned
+ * - for directories:
+ *   - read an integral number of directory entries as in stat
+ *   - offset must be zero or offset + (returned) count of last request (no
+ *     seek to anywhere but beginning of directory)
+ * - more than one message may be produced by a single read call; iounit
+ *   from open/create, if non-zero, gives maximum size guaranteed to be
+ *   returned atomically
+ */
 struct transaction *client_tread(struct transaction *trans) {
     struct Tread *req = &trans->in->msg.tread;
     struct Rread *res = &trans->out->msg.rread;
@@ -482,6 +679,27 @@ struct transaction *client_tread(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_twrite: transfer data to a file
+ *
+ * Arguments:
+ * - fid[4]: file to write to
+ * - offset[8]: position (in bytes) at which to start writing
+ * - count[4]: number of bytes to write
+ * - data[count: bytes to write
+ *
+ * Return:
+ * - count[4]: number of bytes written
+ *
+ * Semantics:
+ * - fid must be open for writing and must not be a directory
+ * - write starts at offset bytes from the beginning of the file
+ * - if file is append-only, offset is ignored and write happens at end of file
+ * - count returned being <= count requested usually indicates an error
+ * - more than one message may be produced by a single read call; iounit
+ *   from open/create, if non-zero, gives maximum size guaranteed to be
+ *   returned atomically
+ */
 struct transaction *client_twrite(struct transaction *trans) {
     struct Twrite *req = &trans->in->msg.twrite;
     struct Rwrite *res = &trans->out->msg.rwrite;
@@ -509,6 +727,19 @@ struct transaction *client_twrite(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tclunk: forget about a fid
+ *
+ * Arguments:
+ * - fid[4]: fid to forget about
+ *
+ * Return:
+ *
+ * Semantics:
+ * - fid is released and can be re-allocated
+ * - if ORCLOSE was set at file open, file will be removed
+ * - even if an error is returned, the fid is no longer valid
+ */
 struct transaction *client_tclunk(struct transaction *trans) {
     struct Tclunk *req = &trans->in->msg.tclunk;
     struct fid *fid;
@@ -526,6 +757,21 @@ struct transaction *client_tclunk(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tremove: remove a file
+ *
+ * Arguments:
+ * - fid[4]: fid of file to remove
+ *
+ * Return:
+ *
+ * Semantics:
+ * - file is removed
+ * - fid is clunked, even if the remove fails
+ * - client must have write permission in parent directory
+ * - if other clients have the file open, file may or may not be removed
+ *   immediately: this is implementation dependent
+ */
 struct transaction *client_tremove(struct transaction *trans) {
     struct Tremove *req = &trans->in->msg.tremove;
     struct fid *fid;
@@ -550,6 +796,18 @@ struct transaction *client_tremove(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_tstat: inquire about a file's attributes
+ *
+ * Arguments:
+ * - fid[4]: fid of file being queried
+ *
+ * Return:
+ * - stat[n]: file stats
+ *
+ * Semantics:
+ * - requires no special permissions
+ */
 struct transaction *client_tstat(struct transaction *trans) {
     struct Tstat *req = &trans->in->msg.tstat;
     struct Rstat *res = &trans->out->msg.rstat;
@@ -563,6 +821,38 @@ struct transaction *client_tstat(struct transaction *trans) {
     return trans;
 }
 
+/**
+ * client_twstat: modify a file's attributes
+ *
+ * Arguments:
+ * - fid[4]: fid of file being modified
+ * - stat[n]: requested file stats
+ *
+ * Return:
+ *
+ * Semantics:
+ * - name can be changed by anyone with write permission in the directory
+ * - name cannot be changed to an existing name
+ * - length can be changed by anyone with write permission on the file
+ * - length of directory can only be set to zero
+ * - server may choose to reject length changes for other reasons
+ * - mode and mtime can be changed by file's owner and by group leader
+ * - directory bit cannot be changed, other mode and perm bits can
+ * - gid can be changed by owner if also a member of new group, or by
+ *   current group leader if also group leader of new group
+ * - no other data can be changed by wstat, and attempts will trigger an error
+ * - it is illegal to attempt to change owner of a file
+ * - changes are atomic: all success or none happen
+ * - request can include "don't touch" values: empty strings for text fields
+ *   and ~0 for unsigned values
+ * - if all fields are "don't touch", file contents should be committed to
+ *   stable storage before the Rwstat is returned
+ * - return value has 3 length fiels:
+ *   - implicit length taken from message size
+ *   - n from stat[n] (number of bytes in stat record)
+ *   - size field in the stat record
+ *   - note that n = size + 2
+ */
 struct transaction *client_twstat(struct transaction *trans) {
     struct Twstat *req = &trans->in->msg.twstat;
     struct fid *fid;
